f87739a22ebc9ff644f96399cfc690c733e26837
Takahir-O <87518531+Takahir-O@users.noreply.github.com>
2022-04-12 22:01:16 +0900

    photo updated

--- scripts.js
+++ scripts.js
@@ -3,12 +3,26 @@ const setting = {
 
 }
 
-class photo {
-    constructor(name,price,imgURL){
+class Photo {
+    constructor(number,name,price,imgURL){
+        this.number = number;
         this.name = name;
         this.price = price;
         this.imgURL = imgURL;
     }
 }
 
-const items = 
+const photo = [
+    new Photo("1","Bird","$10","https://github.com/Takahir-O/recursion-project2/blob/master/images/bird.jpg"),
+    new Photo("2","Building","$9","https://github.com/Takahir-O/recursion-project2/blob/master/images/building.jpg"),
+    new Photo("3","Coffee","$5","https://github.com/Takahir-O/recursion-project2/blob/master/images/coffee.jpg"),
+    new Photo("4","Dog","$3","https://github.com/Takahir-O/recursion-project2/blob/master/images/dog.jpg"),
+    new Photo("5","Flower","$8","https://github.com/Takahir-O/recursion-project2/blob/master/images/flower.jpg"),
+    new Photo("6","Lion","$2","https://github.com/Takahir-O/recursion-project2/blob/master/images/lion.jpg"),
+    new Photo("7","Mountain","$11","https://github.com/Takahir-O/recursion-project2/blob/master/images/mountain.jpg"),
+    new Photo("8","Sea","$15","https://github.com/Takahir-O/recursion-project2/blob/master/images/sea.jpg"),
+    new Photo("9","Shoes","$1","https://github.com/Takahir-O/recursion-project2/blob/master/images/shoes.jpg"),
+    new Photo("10","Watch","$30","https://github.com/Takahir-O/recursion-project2/blob/master/images/watch.jpg")
+
+
+]
